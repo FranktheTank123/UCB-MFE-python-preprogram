311a4c1ff5399ee1b750b70b99002fa958327423
Lisette <ruitong6xiao@gmail.com>
2021-12-30 13:04:49 +0100

    add game_of_life.py

---
+++ game_of_life.py
@@ -0,0 +1,31 @@
+def evolve(m):
+    import numpy
+    return numpy.transpose(m).tolist()
+
+
+test_case_1 = [
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0],
+]
+
+test_case_2 = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+
+test_case_2_next = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+
+evolve(test_case_1) == test_case_1
+
+evolve(test_case_2) == test_case_2_next
