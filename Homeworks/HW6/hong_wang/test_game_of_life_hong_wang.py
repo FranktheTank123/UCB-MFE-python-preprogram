6c50cef56062572eafe292785a1202ea378b408a
billwanghong <billwanghong93@gmail.com>
2022-01-12 00:06:24 +0800

    hw6_hong_wang

--- Homeworks/HW6/hong_wang/test_game_of_life_hong_wang.py
+++ Homeworks/HW6/hong_wang/test_game_of_life_hong_wang.py
@@ -1,60 +1,31 @@
 def evolve(initial_state):
-    # fill this out
-    import numpy as np
+    length = len(initial_state)
+    ans = []
 
-    n_row = np.shape(initial_state)[0]
-    n_col = np.shape(initial_state)[1]
+    for i in range(length):
+        ans1 = []
+        for j in range(length):
+            min_r = max(0, i - 1)
+            max_r = min(length, i + 2)
+            neigh = initial_state[min_r:max_r]
 
-    final_state = [[0] * n_col for i in range(n_row)]
+            min_c = max(0, j - 1)
+            max_c = min(length, j + 2)
+            neigh = map(lambda x: x[min_c:max_c], neigh)
+            neigh = list(neigh)
+            neighbors = []
+            for p in neigh:
+                neighbors.extend(p)
+            num_live = sum(neighbors) - initial_state[i][j]
 
-    for i in range(0, n_row):
-        for j in range(0, n_col):
-            try:
-                a1 = initial_state[i - 1][j - 1]
-            except IndexError:
-                a1 = 0
-            try:
-                a2 = initial_state[i - 1][j]
-            except IndexError:
-                a2 = 0
-            try:
-                a3 = initial_state[i - 1][j + 1]
-            except IndexError:
-                a3 = 0
-            try:
-                a4 = initial_state[i][j - 1]
-            except IndexError:
-                a4 = 0
-            try:
-                a5 = initial_state[i][j + 1]
-            except IndexError:
-                a5 = 0
-            try:
-                a6 = initial_state[i + 1][j - 1]
-            except IndexError:
-                a6 = 0
-            try:
-                a7 = initial_state[i + 1][j]
-            except IndexError:
-                a7 = 0
-            try:
-                a8 = initial_state[i + 1][j + 1]
-            except IndexError:
-                a8 = 0
-
-            if a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 < 2:
-                final_state[i][j] = 0
-            elif a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 > 3:
-                final_state[i][j] = 0
-            elif a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 == 3:
-                final_state[i][j] = 1
-            elif a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 == 2:
-                final_state[i][j] = initial_state[i][j]
-            # print(final_state)
-            # print(a1+a2+a3+a4+a5+a6+a7+a8)
-
-    return final_state
-
+            if initial_state[i][j] == 1 and num_live == 2:
+                ans1.append(1)
+            elif num_live == 3:
+                ans1.append(1)
+            else:
+                ans1.append(0)
+        ans.append(ans1)
+    return ans
     pass
 
 
