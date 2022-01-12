a3fe5d23650b828017fc0549d0251946754ffee3
SF <47039612+catmewo2@users.noreply.github.com>
2022-01-12 15:54:17 -0800

    Reformat test_game_of_life

--- Homeworks/HW6/Siyuan_Fan/test_game_of_life.py
+++ Homeworks/HW6/Siyuan_Fan/test_game_of_life.py
@@ -28,9 +28,9 @@ def test_case_2():
 
 def evolve(initial_state):
     # Create a new matrix to save result
-    next_state = [
-        [0 for x in range(len(initial_state[y]))] for y in range(len(initial_state))
-    ]
+    w = len(initial_state[0])
+    h = len(initial_state)
+    next_state = [[0 for x in range(w)] for y in range(h)]
     # Loop over matrix
     for j in range(len(initial_state)):
         for i in range(len(initial_state[j])):
@@ -38,8 +38,11 @@ def evolve(initial_state):
             next_state[j][i] = initial_state[j][i]
             # If current cell is alive
             if initial_state[j][i] == 1:
-                # Any live cell with two or three live neighbours survives; all other dies
-                if number_of_neighbors_live < 2 or number_of_neighbors_live > 3:
+                # Any live cell with two or three live neighbours survives;
+                # All other dies
+                if number_of_neighbors_live < 2:
+                    next_state[j][i] = 0
+                if number_of_neighbors_live > 3:
                     next_state[j][i] = 0
             # If current cell is dead
             if initial_state[j][i] == 0:
@@ -48,13 +51,11 @@ def evolve(initial_state):
     return next_state
 
 
-def neighbors_sum(initial_state, j, i):
-    left = max(0, i - 1)
-    right = min(len(initial_state[0]), i + 2)
-    top = max(0, j - 1)
-    bottom = min(len(initial_state), j + 2)
+def neighbors_sum(i_s, j, i):
+    e = max(0, i - 1)
+    g = min(len(i_s[0]), i + 2)
+    t = max(0, j - 1)
+    b = min(len(i_s), j + 2)
 
-    neighbours = [
-        [initial_state[r][c] for c in range(left, right)] for r in range(top, bottom)
-    ]
-    return sum([sum(r) for r in neighbours]) - initial_state[j][i]
+    neighbours = [[i_s[r][c] for c in range(e, g)] for r in range(t, b)]
+    return sum([sum(r) for r in neighbours]) - i_s[j][i]
