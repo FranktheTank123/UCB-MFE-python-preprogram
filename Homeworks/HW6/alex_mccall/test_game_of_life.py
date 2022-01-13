41e779f04e3f46a3a13ab7824f8ad90ef5e02b7f
Alexander McCall <alex.mccall255@gmail.com>
2022-01-13 01:46:32 -0500

    add homework 6

---
+++ Homeworks/HW6/alex_mccall/test_game_of_life.py
@@ -0,0 +1,54 @@
+import copy
+
+import pytest
+
+
+def evolve(initial_state):
+    next_state = copy.deepcopy(initial_state)
+    for i, row in enumerate(initial_state):
+        for j, item in enumerate(row):
+            total = 0
+            for vertical in range(max(j - 1, 0), min(j + 2, len(row))):
+                for horizontal in range(max(i - 1, 0), min(i + 2, len(row))):
+                    if (i == horizontal) & (j == vertical):
+                        continue
+                    total += initial_state[horizontal][vertical]
+            if total == 2:
+                pass
+            elif total == 3:
+                next_state[i][j] = 1
+            else:
+                next_state[i][j] = 0
+    return next_state
+
+
+test_case_1 = [
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0],
+]
+
+test_case_2 = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+
+test_case_2_next = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+
+
+@pytest.mark.parametrize(
+    "input_case, output_case",
+    [(test_case_1, test_case_1), (test_case_2, test_case_2_next)],
+)
+def test_evolve(input_case, output_case):
+    assert evolve(input_case) == output_case
