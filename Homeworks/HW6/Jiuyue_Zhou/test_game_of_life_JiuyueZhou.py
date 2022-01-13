a4c79de255f667bb2cb75bca3d6a7b9b96ba4522
youaremylove123 <15801567236@163.com>
2022-01-13 23:19:42 +0800

    hw_6

--- Homeworks/HW6/Jiuyue_Zhou/test_game_of_life_JiuyueZhou.py
+++ Homeworks/HW6/Jiuyue_Zhou/test_game_of_life_JiuyueZhou.py
@@ -8,17 +8,19 @@ def evolve(initial_state):
         row_out = []
         for j in range(n_col):
             n_live_neighbors = 0
-            for ii in range(max(i-1,0), min(i+1,n_row-1) + 1):
-                for jj in range(max(j-1,0), min(j+1,n_col-1) + 1):
+            for ii in range(max(i - 1, 0), min(i + 1, n_row - 1) + 1):
+                for jj in range(max(j - 1, 0), min(j + 1, n_col - 1) + 1):
                     n_live_neighbors += initial_state[ii][jj]
             n_live_neighbors -= initial_state[i][j]
-            if (initial_state[i][j]==1 and n_live_neighbors in [2,3]) or (initial_state[i][j]==0 and n_live_neighbors==3):
+            if (initial_state[i][j] == 1 and n_live_neighbors in [2, 3]) or (
+                initial_state[i][j] == 0 and n_live_neighbors == 3
+            ):
                 row_out.append(1)
             else:
                 row_out.append(0)
         out.append(row_out)
     return out
-    
+
 
 test_case_1 = [
     [0, 0, 0, 0],
@@ -45,12 +47,11 @@ test_case_2_next = [
 
 
 @pytest.mark.parametrize(
-        "input_state, output_state",
-        [
-            (test_case_1, test_case_1),
-            (test_case_2, test_case_2_next),
-        ],
+    "input_state, output_state",
+    [
+        (test_case_1, test_case_1),
+        (test_case_2, test_case_2_next),
+    ],
 )
 def test_evolve(input_state: list, output_state: list):
     assert evolve(input_state) == output_state
-
