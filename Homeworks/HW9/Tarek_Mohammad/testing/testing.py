27026620de21420c8c28da7aaff6815b90cc91a4
Tarek M Mohammad <tarekmmhd@gmail.com>
2022-02-24 12:24:29 +0200

    Adding HW9 Files_Tarek

---
+++ Homeworks/HW9/Tarek_Mohammad/testing/testing.py
@@ -0,0 +1,20 @@
+import pytest
+import sys
+sys.path.append("../real_app/")
+from endpoints import app
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_welcome_page(client):
+    rv = client.get('/')
+    assert 'FINAL PROJECT' in str(rv.data)
+
+
+def test_predict(client):
+    rv = client.get('/2021-11-01 00:00:00')
+    assert 'ret_sol' in str(rv.data)
